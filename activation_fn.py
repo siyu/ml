1cade5496e414421f93f8186741d04b8ae4c310c
siyu <siyu798@gmail.com>
2018-05-29 11:52:05 -0400

    Create activation_fn.py

---
+++ activation_fn.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+
+    ### START CODE HERE ### (≈ 1 line of code)
+    s = 1 / (1 + np.exp(-z))
+    ### END CODE HERE ###
+
+    return s
+
+
+def relu(z):
+    """
+    Compute the relu of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- relu(z)
+    """
+
+    s = np.max(0, z)
+
+    return s
+
+
+def relu_backward(dout, cache):
+  """
+  Computes the backward pass for a layer of rectified linear units (ReLUs).
+  Input:
+  - dout: Upstream derivatives, of any shape
+  - cache: Input x, of same shape as dout
+  Returns:
+  - dx: Gradient with respect to x
+  """
+  dx, x = None, cache
+  #############################################################################
+  # TODO: Implement the ReLU backward pass.                                   #
+  #############################################################################
+  dx = np.array(dout, copy=True)
+  dx[x <= 0] = 0
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+  return dx
+
+
+def sigmoid_backward(dA, cache):
+    """
+    Implement the backward propagation for a single SIGMOID unit.
+    Arguments:
+    dA -- post-activation gradient, of any shape
+    cache -- 'Z' where we store for computing backward propagation efficiently
+    Returns:
+    dZ -- Gradient of the cost with respect to Z
+    """
+
+    Z = cache
+
+    s = 1 / (1 + np.exp(-Z))
+    dZ = dA * s * (1 - s)
+
+    assert (dZ.shape == Z.shape)
+
+    return dZ
